2df7ac624bd0068d37c69205d0b3daa07b8673e3
Syd-ny <sydneymillon@gmail.com>
2023-12-07 11:46:29 +0100

    serializer

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class person(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField(default=0)
+class Pokemon(models.Model):
+    nom = models.CharField(max_length=100)
+    type1 = models.IntegerField(max.length=50)
+    type2 = models.IntegerField(max.length=50, blank=True, null=True)
+    hp = models.IntegerField()
+    attaque = models.IntegerField()
+    defense = models.IntegerField()
+    sp_attaque = models.IntegerField()
+    sp_defense = models.IntegerField()
+    speed = models.IntegerField()
+    recovery = models.IntegerField()
+    generation = models.IntegerField()
     catchphrase = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.nom
+
--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from .models import Pokemon
 from .serializers import PokemonSerializer
+class PokemonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pokemon
+        fields = '__all__'
 
-# Create your views here.
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
