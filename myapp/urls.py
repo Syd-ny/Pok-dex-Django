9439c0489e732a8d831d18e555b372becf803b6e
MathieuOrieux974 <mathieu.orieux@ecoles-epsi.net>
2024-01-11 07:37:01 +0100

    test

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PokemonViewSet
+from .views import PokemonViewSet, pokemon_list, pokemon_detail
 
 router = DefaultRouter()
 router.register(r'pokemon', PokemonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('index/', pokemon_list, name='index'),
+    path('pokemon-detail/<int:pokemon_id>/', pokemon_detail, name='pokemon_detail'),
+
+
     # path('blog/<str:name>/<int:number>',views.blog, name='blog'),
     #path('info/',views.info, name='info'),
     #path('index/',views.index, name='index'),
