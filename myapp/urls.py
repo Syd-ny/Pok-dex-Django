1eda9e43010db7035bf0fbe139360d42154a3b72
MathieuOrieux974 <mathieu.orieux@ecoles-epsi.net>
2024-01-12 21:22:35 +0100

    ajout afficher et ajouter équipe

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PokemonViewSet, pokemon_list, pokemon_detail
+from .views import PokemonViewSet, pokemon_list, pokemon_detail, ajout_equipe, affichage_equipe
 
 router = DefaultRouter()
 router.register(r'pokemon', PokemonViewSet)
@@ -10,7 +10,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('index/', pokemon_list, name='index'),
     path('pokemon-detail/<int:pokemon_id>/', pokemon_detail, name='pokemon_detail'),
-
+    path('ajout-equipe/', ajout_equipe, name='ajout_equipe'),
+    path('affichage-equipe/', affichage_equipe, name='affichage_equipe'),
 
     #path('blog/<str:name>/<int:number>',views.blog, name='blog'),
     #path('info/',views.info, name='info'),
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from .models import Pokemon
 from .serializers import PokemonSerializer
@@ -23,9 +23,46 @@ def pokemon_list(request):
     return render(request, 'index.html', {'pokemon_data': pokemon_data})
 
 def pokemon_detail(request, pokemon_id):
-
     # Récupérer le Pokémon en fonction de l'ID
-    pokemon = Pokemon.objects.get(pk=pokemon_id)
+    pokemon = get_object_or_404(Pokemon, pk=pokemon_id)
 
-    # Passer le Pokémon au template
-    return render(request, 'pokemon_detail.html', {'pokemon': pokemon})
+    # Trouver l'ID du Pokémon précédent
+    previous_pokemon = Pokemon.objects.filter(id__lt=pokemon.id).order_by('-id').first()
+
+    # Trouver l'ID du Pokémon suivant
+    next_pokemon = Pokemon.objects.filter(id__gt=pokemon.id).order_by('id').first()
+
+    context = {
+        'pokemon': pokemon,
+        'previous_pokemon': previous_pokemon,
+        'next_pokemon': next_pokemon,
+    }
+
+    # Passer le Pokémon et les informations sur le précédent et suivant au template
+    return render(request, 'pokemon_detail.html', context)
+
+# myapp/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Pokemon, Equipe
+
+@login_required
+def ajout_equipe(request):
+    if request.method == 'POST':
+        nom_equipe = request.POST['nom_equipe']
+        pokemons_ids = request.POST.getlist('pokemons')
+        dresseur = request.user
+
+        equipe = Equipe.objects.create(nom=nom_equipe, dresseur=dresseur)
+        equipe.pokemons.set(pokemons_ids)
+
+        return redirect('affichage_equipe')
+
+    pokemons = Pokemon.objects.all()
+    return render(request, 'ajout_equipe.html', {'pokemons': pokemons})
+
+@login_required
+def affichage_equipe(request):
+    equipes = Equipe.objects.filter(dresseur=request.user)
+    return render(request, 'affichage_equipe.html', {'equipes': equipes})
